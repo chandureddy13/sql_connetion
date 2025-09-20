c04e8fa1a6b97d3c02a7bb39dacff2b84d539149
chandureddy031 <chandureddy2579@gmail.com>
2025-09-20 10:31:39 +0530

    Initial commit - Job Form App (safe, no credentials)

---
+++ db_config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+import mysql.connector
+
+load_dotenv()  # load variables from .env file
+
+def get_connection():
+    return mysql.connector.connect(
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DB")
+    )
